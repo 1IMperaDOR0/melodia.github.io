c77599aa23bef618293ba35ec9b6b8f06e9c9adb
Matheus Santos <Matheus.soliveira11@hotmail.com>
2025-09-21 13:37:15 -0300

    deixando os benefícios mais bonito

--- react/src/pages/contato/Contato.tsx
+++ react/src/pages/contato/Contato.tsx
@@ -20,7 +20,7 @@ function Contato() {
         </div>
 
         <div className="bg-[#7B00FF] w-[100vw] text-white p-15 flex flex-col justify-center items-center text-center">
-                <h2 className="text-4xl font-bold mb-5">Envie sua mensagem</h2>
+                <h2 className="text-4xl font-bold mb-5">ENVIE SUA MENSAGEM</h2>
                 <p className="max-w-150 mb-10">Preencha o formulário abaixo e responderemos o mais rápido possível.</p>
                 <form className="bg-white rounded-2xl p-10 w-[90%] md:w-[60%] lg:w-[40%] text-black">
                     <input 
@@ -45,9 +45,9 @@ function Contato() {
                     </button>
                 </form>
             </div>
-            <div className="bg-[#FF2C63] w-[100vw] text-white py-20 flex flex-col justify-center items-center text-center">
-                <h2 className="text-3xl font-bold mb-5">Outros canais</h2>
-                <p className="max-w-150 mb-10">Você também pode nos encontrar nas redes sociais ou diretamente em nosso e-mail.</p>
+            <div className="bg-[#FF2C63] w-[100vw] text-[#7B00FF] py-20 flex flex-col justify-center items-center text-center">
+                <h2 className="text-3xl font-bold text-white mb-5">OUTROS CANAIS</h2>
+                <p className="max-w-150 text-white mb-10">Você também pode nos encontrar nas redes sociais ou diretamente em nosso e-mail.</p>
                 <div className="flex gap-5">
                     <a href="#" className="px-6 py-3 bg-[#4AFF4A] rounded-full">Facebook</a>
                     <a href="#" className="px-6 py-3 bg-[#4AFF4A] rounded-full">Instagram</a>
