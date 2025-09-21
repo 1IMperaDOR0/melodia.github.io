63f235b108bfa18c6be49353f5a0cbe81b31837a
Henrique de Oliveira <henrique2801oliveira@gmail.com>
2025-09-21 00:19:55 -0300

    Adicionando primeiro setor de Novidades

--- react/src/pages/musicas/Musicas.tsx
+++ react/src/pages/musicas/Musicas.tsx
@@ -1,7 +1,26 @@
 function Musicas() {
     return (
         <>
-            <p>Opa</p>
+            <div className="p-15">
+                <p className="text-2xl">Novidades</p>
+                <div className="flex mt-3 flex-wrap gap-3">
+                    <div className="border border-gray-100 w-95 h-60 rounded-lg">
+                        <p className="text-black text-xl ml-1">Natural</p>
+                        <p className="text-gray-800 ml-1">Imagine Dragons</p>
+                        <img src="https://imgs.search.brave.com/TjWAfxCLb5SjnvoEzP3ZZwu9vD9Ndzr37qiXkHqMG-Q/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pMS5z/bmRjZG4uY29tL2Fy/dHdvcmtzLVFhTDRG/a05YRHZCOHRXdWot/WDF1NjVnLXQ1MDB4/NTAwLmpwZw" alt="" className="w-95 h-45"/>
+                    </div>
+                    <div className="border border-gray-100 w-95 h-60 rounded-lg">
+                        <p className="text-black text-xl ml-1">Natural</p>
+                        <p className="text-gray-800 ml-1">Imagine Dragons</p>
+                        <img src="https://imgs.search.brave.com/TjWAfxCLb5SjnvoEzP3ZZwu9vD9Ndzr37qiXkHqMG-Q/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pMS5z/bmRjZG4uY29tL2Fy/dHdvcmtzLVFhTDRG/a05YRHZCOHRXdWot/WDF1NjVnLXQ1MDB4/NTAwLmpwZw" alt="" className="w-95 h-45"/>
+                    </div>
+                    <div className="border border-gray-100 w-95 h-60 rounded-lg">
+                        <p className="text-black text-xl ml-1">Natural</p>
+                        <p className="text-gray-800 ml-1">Imagine Dragons</p>
+                        <img src="https://imgs.search.brave.com/TjWAfxCLb5SjnvoEzP3ZZwu9vD9Ndzr37qiXkHqMG-Q/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pMS5z/bmRjZG4uY29tL2Fy/dHdvcmtzLVFhTDRG/a05YRHZCOHRXdWot/WDF1NjVnLXQ1MDB4/NTAwLmpwZw" alt="" className="w-95 h-45"/>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
