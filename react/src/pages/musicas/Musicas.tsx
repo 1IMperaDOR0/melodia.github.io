d8dd621d4dcedf0829ecd25d4e1be37ff5c0f499
Henrique de Oliveira <henrique2801oliveira@gmail.com>
2025-09-21 02:11:44 -0300

    Fixing music lists and album section

--- react/src/pages/musicas/Musicas.tsx
+++ react/src/pages/musicas/Musicas.tsx
@@ -1,9 +1,21 @@
+{/*Capa de músicas */}
 import natural from "../../assets/images/natural.webp";
 import mtr from "../../assets/images/mtr.webp";
 import rise from "../../assets/images/rise.webp";
 import empitness from "../../assets/images/emptiness.webp";
 import slavic from "../../assets/images/slavixqueen.webp";
 import wickedgame from "../../assets/images/wickedgame.webp";
+import radioactive from "../../assets/images/radioactive.jpg";
+import mockingbird from "../../assets/images/mockingbird.jpg";
+{/*Capa de músicas */}
+import album1 from "../../assets/images/album1.jpg";
+import album2 from "../../assets/images/album2.jpg";
+import album3 from "../../assets/images/album3.jpg";
+import album4 from "../../assets/images/album4.jpg";
+import album5 from "../../assets/images/album5.jpg";
+import album6 from "../../assets/images/album6.jpg";
+import album7 from "../../assets/images/album7.jpg";
+
 
 function Musicas() {
     return (
@@ -36,64 +48,64 @@ function Musicas() {
                 <div>
                     <div className="h-auto flex gap-3 justify-center bg-[#460091] p-2 rounded-lg">
                         <div>
-                            <div className="w-80 flex">
+                            <div className="w-80 flex hover:bg-[#542685]">
                                 <img src={natural} alt="" className="w-10 h-10" />
                                 <p className="mt-2 ml-1 text-white">Natural - Imagine Dragons</p>
                             </div>
                             <div className="w-90 h-0.5 bg-[#370072] mb-1 mt-1"></div>
-                            <div className="w-80 flex">
+                            <div className="w-80 flex hover:bg-[#542685]">
                                 <img src={rise} alt="" className="w-10 h-10" />
                                 <p className="mt-2 ml-1 text-white">Rise - Skillet</p>
                             </div>
                             <div className="w-90 h-0.5 bg-[#370072] mb-1 mt-1"></div>
-                            <div className="w-80 flex">
+                            <div className="w-80 flex hover:bg-[#542685]">
                                 <img src={mtr} alt="" className="w-10 h-10" />
                                 <p className="mt-2 ml-1 text-white">Miss The Rage - Jeethz</p>
                             </div>
                             <div className="w-90 h-0.5 bg-[#370072] mb-1 mt-1"></div>
-                            <div className="w-80 flex">
+                            <div className="w-80 flex hover:bg-[#542685]">
                                 <img src={empitness} alt="" className="w-10 h-10" />
                                 <p className="mt-2 ml-1 text-white">The Emptiness Machine - Linkin Park</p>
                             </div>
                         </div>
                         <div className="hidden md:block">
-                            <div className="w-80 flex">
+                            <div className="w-80 flex hover:bg-[#542685]">
                                 <img src={slavic} alt="" className="w-10 h-10" />
                                 <p className="mt-2 ml-1 text-white">Slavic Queen - Filip Lackovic</p>
                             </div>
                             <div className="w-90 h-0.5 bg-[#370072] mb-1 mt-1"></div>
-                            <div className="w-80 flex">
+                            <div className="w-80 flex hover:bg-[#542685]">
                                 <img src={wickedgame} alt="" className="w-10 h-10" />
                                 <p className="mt-2 ml-1 text-white">Wicked Game - Chris Isaak</p>
                             </div>
                             <div className="w-90 h-0.5 bg-[#370072] mb-1 mt-1"></div>
-                            <div className="w-80 flex">
-                                <img src="" alt="" className="w-10 h-10" />
-                                <p className="mt-2 ml-1 text-white">==================</p>
+                            <div className="w-80 flex hover:bg-[#542685]">
+                                <img src={radioactive} alt="" className="w-10 h-10" />
+                                <p className="mt-2 ml-1 text-white">Radioactive - Imagine Dragons</p>
                             </div>
                             <div className="w-90 h-0.5 bg-[#370072] mb-1 mt-1"></div>
-                            <div className="w-80 flex">
-                                <img src="" alt="" className="w-10 h-10" />
-                                <p className="mt-2 ml-1 text-white">==================</p>
+                            <div className="w-80 flex hover:bg-[#542685]">
+                                <img src={mockingbird} alt="" className="w-10 h-10" />
+                                <p className="mt-2 ml-1 text-white">Mockingbird - Eminem</p>
                             </div>
                         </div>
                         <div className="hidden lg:block">
-                            <div className="w-80 flex">
+                            <div className="w-80 flex hover:bg-[#542685]">
                                 <img src="" alt="" className="w-10 h-10" />
                                 <p className="mt-2 ml-1 text-white">==================</p>
                             </div>
                             <div className="w-90 h-0.5 bg-[#370072] mb-1 mt-1"></div>
-                            <div className="w-80 flex">
+                            <div className="w-80 flex hover:bg-[#542685]">
                                 <img src="" alt="" className="w-10 h-10" />
                                 <p className="mt-2 ml-1 text-white">==================</p>
                             </div>
                             <div className="w-90 h-0.5 bg-[#370072] mb-1 mt-1"></div>
-                            <div className="w-80 flex">
+                            <div className="w-80 flex hover:bg-[#542685]">
                                 <img src="" alt="" className="w-10 h-10" />
                                 <p className="mt-2 ml-1 text-white">==================</p>
                             </div>
                             <div className="w-90 h-0.5 bg-[#370072] mb-1 mt-1"></div>
-                            <div className="w-80 flex">
+                            <div className="w-80 flex hover:bg-[#542685]">
                                 <img src="" alt="" className="w-10 h-10" />
                                 <p className="mt-2 ml-1 text-white">==================</p>
                             </div>
@@ -101,8 +113,38 @@ function Musicas() {
                     </div>
                 </div>
             </div>
-            <div className="bg-[#FF2C63] h-auto p-15">
-                <p>Lançamentos de Álbuns &gt;</p>
+            <div className="bg-[#FF2C63] h-auto py-15">
+                <p className="text-2xl mb-3 ml-15">Playlists &gt;</p>
+                <div className="flex flex-wrap gap-3 ml-15">
+                    <div className="flex flex-col w-45">
+                        <img src={album1} alt="" className="w-45 h-45" />
+                        <p>Novidades do dia</p>
+                    </div>
+                    <div className="flex flex-col w-45">
+                        <img src={album2} alt="" className="w-45 h-45" />
+                        <p>Marília Mendonça</p>
+                    </div>
+                    <div className="flex flex-col w-45">
+                        <img src={album3} alt="" className="w-45 h-45" />
+                        <p>Só sertanejo</p>
+                    </div>
+                    <div className="flex flex-col w-45">
+                        <img src={album4} alt="" className="w-45 h-45" />
+                        <p>Racionais</p>
+                    </div>
+                    <div className="flex flex-col w-45">
+                        <img src={album5} alt="" className="w-45 h-45" />
+                        <p>O Rei 👑</p>
+                    </div>
+                    <div className="flex flex-col w-45">
+                        <img src={album6} alt="" className="w-45 h-45" />
+                        <p>Sétimo Som</p>
+                    </div>
+                    <div className="flex flex-col w-45">
+                        <img src={album7} alt="" className="w-45 h-45" />
+                        <p>Skillet</p>
+                    </div>
+                </div>
             </div>
         </>
     )
